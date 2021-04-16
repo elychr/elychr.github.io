6bb61c73e8bb1ada37e09912fc2238f4e90fefef
elychr <51218224+elychr@users.noreply.github.com>
2021-04-16 16:59:11 +1200

    Fixed routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import * as serviceWorker from './components/serviceWorker'
 import Sidebar from './components/sidebar'
